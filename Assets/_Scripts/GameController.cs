17e075ec3e8f74fbc5113bd7ed883f55241a7342
Gurinder Verma <Gurinder Verma>
2016-03-02 20:09:22 -0500

    third commit

---
+++ Assets/_Scripts/GameController.cs
@@ -0,0 +1,27 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+public class GameController : MonoBehaviour {
+	// PUBLIC INSTANCE VARIABLES
+	public int tankCount;
+	public GameObject tank;
+	
+	// Use this for initialization
+	void Start () {
+		this._GenerateTanks ();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+	// generate tanks
+	private void _GenerateTanks() {
+		for (int count=0; count < this.tankCount; count++) {
+			Instantiate(tank);
+		}
+	}
+}
